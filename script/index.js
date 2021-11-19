47ed4e01701dcc697d1c4bebb776cc54770520d7
lesantxs <l.sancarmo@gmail.com>
2021-11-19 04:24:59 -0300

    adicionando javascript ao projeto

---
+++ script/index.js
@@ -0,0 +1,15 @@
+function iniciarEscolha(){
+    var personagens = ""
+    while(personagens != 1 && personagens != 2 && personagens != 3){
+        personagens = prompt("*Present Mic se prepara* \n \n— Manda aííí!! \n \nEscolha o número do personagem: \n1 - Ladra \n2 - Segurança \n3 - Visita")
+        if(personagens == 1){
+            window.location.href = "./ladra/ladra.html"
+        }else if(personagens == 2){
+            window.location.href = "./seguranca/seguranca.html"
+        }else if(personagens == 3){
+            window.location.href = "./visita/visita.html"
+        }else{
+            alert("Errou, jovem! Digite um personagem válido.")
+        }
+    }
+}

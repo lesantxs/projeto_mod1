980feefc0c87179c30e5a4f3363d21d92003a44d
lesantxs <l.sancarmo@gmail.com>
2021-11-20 20:29:26 -0300

    adicionando mudanca de formatacao

--- script/index.js
+++ script/index.js
@@ -1,14 +1,14 @@
-function iniciarEscolha(){
+function iniciarEscolha() {
     var personagens = ""
-    while(true){
+    while (true) {
         personagens = prompt("*Present Mic se prepara* \n \n— Manda aííí!! \n \nEscolha o número do personagem: \n1 - Ladra \n2 - Segurança \n3 - Visita")
-        if(personagens == 1){
+        if (personagens == 1) {
             window.location.href = "./ladra/ladra.html"
-        }else if(personagens == 2){
+        } else if (personagens == 2) {
             window.location.href = "./seguranca/seguranca.html"
-        }else if(personagens == 3){
+        } else if (personagens == 3) {
             window.location.href = "./visita/visita.html"
-        }else{
+        } else {
             alert("Errou, jovem! Digite um personagem válido.")
         }
         break
